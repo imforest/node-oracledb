0dca6d23e3d8b0148334d88ce1233f0d1f189477
Kibeom Kim <imforest@gmail.com>
2022-07-10 08:54:03 +0900

    Add select and insert data function
    - Node.js oracledb를 통한 오라클 연결
    - https://meyouus.tistory.com/66?category=846717

---
+++ config/dbConfig.js
@@ -0,0 +1,6 @@
+/*  DB Info */
+module.exports = {
+    user: process.env.NODE_ORACLEDB_USER || "ADMIN",
+    password: process.env.NODE_ORACLEDB_PASSWOR || "#KviYz*x!F6CUPW",
+    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "db202203162338_medium"
+}
